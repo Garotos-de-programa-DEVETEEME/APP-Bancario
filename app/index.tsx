9f110bb39b1ec0e6a3cc815482353622f5e445ee
Lucas_397 <lu.machado.m397@gmail.com>
2025-07-22 13:43:50 -0300

    feat/initial-pages

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+export default function Index() {
+    return (
+        <>
+            <Text>
+                This is the index page of the app.
+            </Text>
+        </>
+    );
+}
