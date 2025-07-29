8a75fd477aa18b0d0ab9a9fdc41bd04375c3d148
Karmotrineko <nekomaid@outlook.jp>
2025-07-28 21:23:41 -0300

    Tela inicial prototipo

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,46 @@
-import { Text } from "react-native";
+import EntrarButton from '@/components/EntrarButton';
+import { Styles } from '@/constants/Colors';
+import {
+    ImageBackground,
+    SafeAreaView,
+    StyleSheet,
+    View,
+} from 'react-native';
 
-export default function Index() {
-    return (
-        <>
-            <Text>
-                This is the index page of the app.
-            </Text>
-        </>
-    );
+export default function App() {
+  const theme = 'light'; // ou useColorScheme() para detectar dinamicamente
+  const currentStyle = Styles[theme];
+
+  return (
+    <ImageBackground
+      source={'https://legacy.reactjs.org/logo-og.png'}
+      style={styles.background}
+      resizeMode="cover"
+    >
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.content} />
+
+        <EntrarButton
+          title="Cliente"
+          theme={theme}
+          onPress={() => console.log('Entrar clicado')}
+        />
+      </SafeAreaView>
+    </ImageBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+  safeArea: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 40, // espaço entre botão e fundo da tela
+  },
+  content: {
+    flex: 1,
+  },
+});
