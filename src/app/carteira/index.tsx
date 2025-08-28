d158bbac12834eb3427c261005eecc60b55b1ac4
Lucas_397 <lu.machado.m397@gmail.com>
2025-08-28 14:25:09 -0300

    feat(fundos investidos card) criacao do componente de listagem de fundos investidos na pagina de carteira

--- src/app/carteira/index.tsx
+++ src/app/carteira/index.tsx
@@ -1,33 +1,36 @@
 import { StylesType } from '@/src/@Types/stylesType';
 import { WalletInfoCard } from '@/src/components/Wallet/carteira';
+import { FundoInvestido } from '@/src/components/Wallet/fundoInvestido';
 import { MOCK_FUNDOS } from '@/src/data/fundos';
 import { useTheme } from '@/src/hooks/useTheme';
 import { useEffect, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 export default function WalletPage() {
   const theme = useTheme();
   const styles = getStyles(theme);
-  const [fundosInvestidos, setFundosInvestidos] = useState<any[]>(
-    [],
-  ); //TODO fazer integração com base em API
+  const [fundosInvestidos, setFundosInvestidos] = useState<any[]>([]); //TODO fazer integração com base em API
   useEffect(() => {
     setFundosInvestidos(MOCK_FUNDOS);
   }, []);
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
       <View style={styles.cateira}>
         <WalletInfoCard fundosInvestidos={fundosInvestidos} />
       </View>
       <View>
-        <View>
-          {fundosInvestidos.map((e) => {
-            return <>{/*TODO criar componente de fundo investido */}</>;
+        <View style={styles.fundosInvestidos}>
+          {fundosInvestidos.map((e, index) => {
+            return (
+              <>
+                <FundoInvestido key={index} fundoData={e} />
+              </>
+            );
           })}
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -37,7 +40,7 @@ const getStyles = (theme: StylesType) => {
       backgroundColor: theme.background,
       display: 'flex',
       flexDirection: 'column',
-      gap: 13,
+      gap: 14,
     },
     cateira: {
       backgroundColor: theme.backgroundCards,
@@ -45,10 +48,24 @@ const getStyles = (theme: StylesType) => {
       width: '90%',
       alignSelf: 'center',
       borderRadius: 15,
+      marginBottom:12
     },
     patrimonio: {
       borderBottomColor: theme.border,
       borderBottomWidth: 1,
+    },
+    fundosInvestidos: {
+      borderWidth: 1,
+      borderColor: theme.border,
+      borderRadius: 15,
+      paddingTop: 56,
+      paddingBottom: 15,
+      paddingHorizontal: 15,
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 20,
+      width:'90%',
+      alignSelf:'center'
     },
   });
 };
