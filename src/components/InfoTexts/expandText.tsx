e9836fc816d7ea31a8ba395f0ca92564adb072c9
Lucas Machado <lu.machado.m397@gmail.com>
2025-10-01 11:31:37 -0300

    fix(color-text-theme) corrige a cor do texto na tela de saiba mais conforme o tema

--- src/components/InfoTexts/expandText.tsx
+++ src/components/InfoTexts/expandText.tsx
@@ -22,13 +22,13 @@ export const ExpandedText = ({title, expandedItens, defaultExpanded=false}:Expan
             <Pressable onPress={() => setExpanded(prev => !prev)}>
                 <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', gap:4}}>
                     <MaterialCommunityIcons name={expanded?"menu-down":"menu-right"} color={theme.text} size={24} />
-                    <StyledText className="font-bold text-[16px]">{title}</StyledText>
+                    <StyledText className="font-bold text-[16px]" style={{color:theme.text}}>{title}</StyledText>
                 </View>
             </Pressable>
             {expanded && (
                 <View style={{marginTop: 12, gap: 8, marginHorizontal:20}}>
                     {expandedItens.map((item, index)=>(
-                        <TextRow key={index} themeText={theme.text} left={item.left} right={item.right? item.right:'Não há'} />
+                        <TextRow key={index} left={item.left} right={item.right? item.right:'Não há'} />
                     ))}
                 </View>
             )}
--- src/components/InfoTexts/rowText.tsx
+++ src/components/InfoTexts/rowText.tsx
@@ -1,21 +1,22 @@
 import { View } from "react-native";
 import { StyledText } from "../StyledText";
+import { useTheme } from "@/src/hooks/useTheme";
 
 interface TextRowProps{
-    themeText: string; 
     left: string; 
     right: string | number,
     fontSize?: number
     bold?:boolean
 }
 
-export function TextRow({ themeText, left, right, fontSize=16, bold=false }: TextRowProps) {
+export function TextRow({left, right, fontSize=16, bold=false }: TextRowProps) {
+  const theme = useTheme();
   return (
-    <View className="flex-row items-center justify-between border-b border-b-gray-300 pb-2 mb-2">
-      <StyledText style={{ color: themeText, fontSize:fontSize, fontWeight:bold?'bold':'300' }}>
+    <View className="flex-row items-center justify-between border-b pb-2 mb-2" style={{borderBottomColor:theme.border, borderBottomWidth:1}}>
+      <StyledText style={{ color: theme.text, fontSize:fontSize, fontWeight:bold?'bold':'300' }}>
         {left}
       </StyledText>
-      <StyledText style={{ color: themeText, fontSize:fontSize }}>
+      <StyledText style={{ color: theme.text, fontSize:fontSize }}>
         {right}
       </StyledText>
     </View>
