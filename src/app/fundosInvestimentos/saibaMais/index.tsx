979a40d29d73b382f3d1e67ae08cd5d32959448a
Lucas Machado <lu.machado.m397@gmail.com>
2025-10-01 13:17:54 -0300

    feat(basic construction) construção inicial da pagina de saiba mais

--- src/app/fundosInvestimentos/saibaMais/index.tsx
+++ src/app/fundosInvestimentos/saibaMais/index.tsx
@@ -1,53 +1,69 @@
 import { FundoInvestimento } from '@/src/@Types/fundos';
 import { StylesType } from '@/src/@Types/stylesType';
 import { DataLess } from '@/src/components/Dataless';
+import { ExpandedText } from '@/src/components/InfoTexts/expandText';
+import { TextRow } from '@/src/components/InfoTexts/rowText';
 import { StyledText } from '@/src/components/StyledText';
 import { useTheme } from "@/src/hooks/useTheme";
+import { coinFormat } from '@/src/utils/coinFormat';
 import { useLocalSearchParams } from 'expo-router';
 import { ScrollView, StyleSheet, View } from 'react-native';
 
 export default function SaibaMais() {
   const { fundData } = useLocalSearchParams();
-   const fund: FundoInvestimento | null =
+   const fundo: FundoInvestimento | null =
     typeof fundData === 'string' ? JSON.parse(fundData) : null;
   const theme = useTheme();
   const styles = getStyles(theme);
+
+  const fundInformation = fundo !== null? [
+    { left: 'Aplicação inicial', right: coinFormat(fundo.valorAplicacaoInicial) },
+    { left: 'Investimento adicional mínimo', right: fundo.valorAplicacaoInicial },
+    { left: 'Resgate mínimo', right: fundo.valorMinimoResgatavel },
+    { left: 'Saldo mínimo para permanência', right: fundo.valorSaldoMinimo },
+    { left: 'Tipo de cota', right: 'Abertura' },
+    { left: 'Carência', rigth: fundo.dataCarenciaResgate },
+    { left: 'Cota de aplicação', right: 'D+0' },
+    { left: 'Cota de resgate', right: `D+${fundo.prazoConversaoResgate}` },
+    { left: 'Débito em conta corrente', right: `D+${fundo.prazoConversaoResgate}` },
+    { left: 'Crédito em conta corrente', right: `D+${fundo.fundoPrazoCreditoConta}` },
+    { left: 'Taxa de gestão', right: `${fundo.taxaAdministracao}% a.a.` },
+    { left: 'Taxa de distribuição', right:  `40% a.a. ` },
+    { left: 'Taxa de performance', rigth:`${fundo.taxaRentabilidade}` },
+    { left: 'Taxa de ingresso',  },
+    { left: 'Taxa de saída', rigth:`${fundo.taxaRentabilidade}` },
+    { left: 'Horário limite para aplicação e resgate', rigth:`${fundo.horaLimiteAplicacaoInternet}:00` },
+  ]:[];
   return (
-    <>
-      {fund !== null? (
-        <ScrollView>
-          <StyledText>{fund.nome}</StyledText>
-          <View>
+    <View style={styles.container}>
+      {fundo !== null? (
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <StyledText style={{fontSize:28, fontWeight:'500', color:theme.text}}>{fundo.nome}</StyledText>
+          <View style={{height:100}}>
             {/* TODO componente grafico */}
           </View>
           <View>
-            <View style={styles.textContainer}>
-              <StyledText style={{color:theme.text, fontSize:16, fontWeight:'bold'}}>Saldo líquido</StyledText>
-              <StyledText style={{color:theme.text, fontSize:15, fontWeight:'300'}}>{fund.valorSaldoMinimo}</StyledText>{/*TODO alterar para saldo liquido */}
-            </View>
-            <View style={styles.textContainer}>
-              <StyledText style={{color:theme.text, fontSize:16, fontWeight:'bold'}}>Classificação de risco</StyledText>
-              <StyledText style={{color:theme.text, fontSize:15, fontWeight:'300'}}> {'Muito Baixo'} </StyledText> {/*TODO alterar conforme API de risco */}
-            </View>
-            <View style={styles.textContainer}>
-              <StyledText style={{color:theme.text, fontSize:16, fontWeight:'bold'}}>Classificação CVM</StyledText>
-              <StyledText style={{color:theme.text, fontSize:15, fontWeight:'300'}}> {'Renda Fixa Simples'} </StyledText>{/*TODO altera conforme tipo do fundo */}
-            </View>
-            <View style={styles.textContainer}>
-              <StyledText style={{color:theme.text, fontSize:16, fontWeight:'bold'}}>Subclasse CVM</StyledText>
-              <StyledText style={{color:theme.text, fontSize:15, fontWeight:'300'}}> {'Renda Fixa Simples'} </StyledText>{/*TODO altera conforme tipo do fundo */}
-            </View>
-            <View style={styles.textContainer}>
-              <StyledText style={{color:theme.text, fontSize:16, fontWeight:'bold'}}>Tipo ANBIMA</StyledText>
-              <StyledText style={{color:theme.text, fontSize:15, fontWeight:'300'}}> {'Renda Fixa Simples'} </StyledText>{/*TODO altera conforme tipo do fundo */}
-            </View>
-
+              <TextRow left={'Saldo líquido'} right={coinFormat(fundo.valorAplicacaoInicial)}  bold />
+              <TextRow left={'Classificação de risco'} right={'Muito Baixo'}  bold />
+              <TextRow left={'Classificação CVM'} right={'Renda Fixa Simples'}  bold />
+              <TextRow left={'Subclassse CVM'} right={'Renda Fixa Simples'}  bold />
+              <TextRow left={'Tipo ANBIMA'} right={'Renda Fixa Simples'}  bold />
+          </View>
+          <View style={{borderBottomColor: theme.border, borderBottomWidth: 1, marginBottom: 16 }}>
+            <ExpandedText title='Condições Comerciais' expandedItens={fundInformation}/>
+            <ExpandedText title='Documentos do fundo' expandedItens={[]}/>
+            <ExpandedText title='Comunicado aos Cotistas' expandedItens={[]}/>
+            <ExpandedText title='Avisos Importantes' expandedItens={[]}/>
+          </View>
+          <View style={{marginTop: 24, marginBottom: 12}}>
+            <StyledText style={{fontSize: 32, fontWeight: 'bold', color:theme.text}}>Sobre o fundo</StyledText>
+            <StyledText style={{fontSize: 32, fontWeight: 'bold', color:theme.text}}> {/*TODO adicionar descrição */} </StyledText>
           </View>
         </ScrollView>
       ):(
         <DataLess/>
       )}
-    </>
+    </View>
   );
 }
 
@@ -57,9 +73,9 @@ const getStyles = (theme: StylesType) => {
       backgroundColor: theme.background,
     },
     container: {
-      flexGrow: 1,
-      paddingHorizontal: 25,
-      paddingVertical: 20,
+      backgroundColor: theme.background,
+      paddingHorizontal:24,
+      paddingTop:16
     },
     containerError: {
       flex: 1,
@@ -77,11 +93,5 @@ const getStyles = (theme: StylesType) => {
       alignItems: 'center',
       gap: 4,
     },
-    textContainer:{
-      justifyContent:'space-between', 
-      display:'flex', 
-      flexDirection:'row'
-    },
   });
 };
-
