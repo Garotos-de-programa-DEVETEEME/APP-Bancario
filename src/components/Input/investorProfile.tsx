88cf13faf0ad0642b0a3c4e8b0249ed84169d42a
Rafael Barbosa Crema <740000314@banestes.sfb>
2025-10-07 15:48:54 -0300

    Colocado texto de perfil dentro da pasta data

--- src/components/Input/investorProfile.tsx
+++ src/components/Input/investorProfile.tsx
@@ -1,4 +1,5 @@
 import { StylesType } from '@/src/@Types/stylesType';
+import { formSteps } from '@/src/data/investidorPerfilPergunta';
 import { useTheme } from "@/src/hooks/useTheme";
 import { useState } from 'react';
 import { Image, Modal, StyleSheet, TouchableOpacity, View } from 'react-native';
@@ -14,29 +15,6 @@ interface InvestorProfileProps {
     clientName: string;
     image: string;
 }
-
-const formSteps = [
-    {
-        id: 1,
-        question: 'Por qual período você deseja manter os seus investimentos?',
-        options: ['Menor que 6 meses.', 'De 6 meses a 1 ano.', 'De 1 a 3 anos.', 'Acima de 3 anos.'],
-    },
-    {
-        id: 2,
-        question: 'Com relação aos riscos envolvidos nos investimentos, como reagiria ao verificar que determinado investimento, após certo período, apresentou retorno negativo?',
-        options: ['Resgataria Imediatamente', 'Determinaria um valor máximo de perda antes de resgatar', 'Realizaria aportes adicionais', 'Só resultados de longo prazo preocupariam'],
-    },
-    {
-        id: 3,
-        question: 'Qual valor atual da sua renda mensal?',
-        options: ['Até R$2 Mil.', 'De R$2 mil a R$5 mil', 'Mais de R$5 mil até R$10 mil', 'Acima de R$10 mil.'],
-    },
-    {
-        id: 4,
-        question: 'Qual valor aproxima do seu patrimônio?',
-        options: ['Até R$100 mil.', 'Entre R$100 mil e R$500 mil', 'Entre R$500 mil e R$1 milhão', 'Acima de R$1 milhão'],
-    }
-];
 
 export function InvestorProfile({ visible, onClose, onAccept, clientName, image }: InvestorProfileProps) {
     const theme = useTheme();
