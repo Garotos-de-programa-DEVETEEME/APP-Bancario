2bd8bf2523bff407a8f71381df9d19b496f16d71
Lucas Machado <lu.machado.m397@gmail.com>
2025-10-24 12:43:44 -0300

    feat(switchRow component) cria componente de row do switch

---
+++ components/ClientProfile/switchRow.tsx
@@ -0,0 +1,64 @@
+import { Entypo, MaterialIcons } from "@expo/vector-icons"
+import { View, StyleSheet } from "react-native"
+import { StyledText } from "../StyledText"
+import { SwitchButton } from "../Buttons/switch"
+import { useTheme } from "@/hooks/useTheme";
+import { StylesType } from "@/@Types/stylesType";
+
+interface SwitchRowProps{
+    title: string;
+    iconName: string;
+    switchValue:boolean;
+    setSwitch: (e:boolean) => void;
+    iconSize?:number;
+}
+
+export const SwitchRow = ({title, iconName, switchValue, setSwitch, iconSize=16 }:SwitchRowProps) =>{
+    const theme = useTheme();
+    const style = getStyles(theme);
+
+    return(
+        <View style={style.switchRow}>{/*TODO implementar o alana mode */}
+            <View style={style.textSwitch}>
+                <MaterialIcons name={iconName as any} color={theme.tint} size={iconSize} />
+                <StyledText style={style.title}>{title}</StyledText>
+            </View>
+            <SwitchButton value={switchValue} onValueChange={setSwitch} />
+        </View>
+
+    )
+}
+
+
+const getStyles = (theme:StylesType) => {
+    return StyleSheet.create({
+        container:{
+            display:'flex', 
+            gap:10,
+            marginTop:10,
+        },
+        contentContainer:{
+            width:'5%',
+            alignSelf:'center',
+        },
+        switchRow:{
+            display:'flex',
+            flexDirection:'row',
+            justifyContent:'space-between',
+            alignItems:'center',
+            borderBottomColor:theme.border,
+            borderBottomWidth:1,
+            paddingBottom:12,
+        },
+        textSwitch:{
+            display:'flex',
+            flexDirection:'row',
+            gap:8,
+            alignItems:'center'
+        },
+        title:{
+            color:theme.text,
+            fontSize:16,
+        },
+    })
+}
